8ea8029f7e52350d7d0d562aec6788f85317ccfa
sc-forlife <salemsc7@gmail.com>
2026-05-20 08:31:14 +0200

    Working on the complaint group element border

--- build-a-customer-complaint/script.js
+++ build-a-customer-complaint/script.js
@@ -1,15 +1,59 @@
-const fullNameInput = document.getElementById("full-name");
+//form inputs
 
-function checkInput(e) {
-  if (e.target.checkValidity()) {
-    console.log(e.target.checkValidity());
-    e.target.style.borderColor = "green";
-  } else {
-    console.log(e.target.checkValidity());
-    e.target.style.borderColor = "red";
-  }
+const fullNameInput = document.getElementById("full-name");
+const emailInput = document.getElementById("email");
+const orderNumInput = document.getElementById("order-no");
+const productCodeInput = document.getElementById("product-code");
+const quantityInput = document.getElementById("quantity");
+const complaintsGroupInput = document.getElementById("complaints-group");
+const otherComplaintsInput = document.getElementById("other-complaint");
+const solutionsGroupInput = document.getElementById("solutions-group");
+const otherSolutionsInput = document.getElementById("solution-description");
+
+function validateForm(e) {
+  e.preventDefault();
+
+  //check if inputs not empty
+  const obj = {};
+  obj["full-name"] = fullNameInput.value ? true : false;
+  obj["email"] = emailInput.value ? true : false;
+  obj["order-no"] = orderNumInput.value ? true : false;
+  obj["product-code"] = productCodeInput.value ? true : false;
+  obj["quantity"] = quantityInput.value ? true : false;
+  obj["complaints-group"] = complaintsGroupInput.value ? true : false;
+  obj["other-complaint"] = otherComplaintsInput.value ? true : false;
+  obj["solutions-group"] = solutionsGroupInput.value ? true : false;
+  obj["solution-description"] = otherSolutionsInput.value ? true : false;
+
+  console.log(obj);
+
+  return obj;
 }
 
+//how do we get complaint group value to bring false when nothing is selected , but re-check the fcc instructions
+
+function checkInput(e) {
+  if (e.target.name === "complaint") {
+    console.log("I ran");
+    console.log(e.target.value);
+    const fieldset = document.getElementById("complaints-group");
+    fieldset.style.borderColor = e.target.value ? "green" : "red";
+  }
+  // if (e.target.checkValidity() && e.target.value !== "") {
+  //   console.log(e.target.checkValidity());
+  //   e.target.style.borderColor = "green";
+  // } else {
+  //   console.log(e.target.checkValidity());
+  //   e.target.style.borderColor = "red";
+  // }
+}
+
+//form buttons
 const form = document.querySelector("form");
+const submit = document.getElementById("submit-btn");
 
 form.addEventListener("change", checkInput);
+
+submit.addEventListener("click", (e) => {
+  validateForm(e);
+});
