1907f842537602a27844ead2d59632b862a0be93
sc-forlife <salemsc7@gmail.com>
2026-06-18 06:24:05 +0200

    Working on the findRepeatedPhrases function

--- build-a-Proofreading-tool/script.js
+++ build-a-Proofreading-tool/script.js
@@ -3,3 +3,17 @@ export function isPalindrome(word) {
   const reverseWord = splitWord.reverse().join("");
   return reverseWord === word.toLowerCase();
 }
+
+export function findPalindromeBreaks(wordsArr) {
+  const indices = [];
+  for (let i = 0; i < wordsArr.length; i++) {
+    if (!isPalindrome(wordsArr[i])) {
+      indices.push(i);
+    }
+  }
+  return indices;
+}
+
+export function findRepeatedPhrases(words, phraseLength) {
+  if (phraseLength >= words.length) return [];
+}
--- build-a-Proofreading-tool/script.test.js
+++ build-a-Proofreading-tool/script.test.js
@@ -1,5 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { isPalindrome } from "./script";
+import { findPalindromeBreaks } from "./script";
+import { findRepeatedPhrases } from "./script";
 
 describe("isPalindrome", () => {
   it("Return true if Palindrome and false if not , case-insensitive", () => {
@@ -10,5 +12,46 @@ describe("isPalindrome", () => {
     expect(isPalindrome("madam")).toBeTruthy();
     expect(isPalindrome("noon")).toBeTruthy();
     expect(isPalindrome("civic")).toBeTruthy();
+  });
+});
+
+describe("findPalindromeBreaks", () => {
+  it("return array of indices from palindromes", () => {
+    const palindromeList = findPalindromeBreaks([
+      "level",
+      "salem",
+      "racecar",
+      "hello",
+    ]);
+    expect(palindromeList).toMatchObject([1, 3]);
+  });
+  it("Return empty arrays if input empty", () => {
+    const palindromeListEmpty = findPalindromeBreaks([]);
+    expect(palindromeListEmpty).toMatchObject([]);
+  });
+});
+
+describe("findRepeatedPhrases", () => {
+  it("Return array of start of indices of repeating phrases", () => {
+    const indices = findRepeatedPhrases(
+      ["the", "cat", "sat", "the", "cat", "ran"],
+      2,
+    );
+    expect(indices).toMatchObject([0, 3]);
+
+    const words = findRepeatedPhrases(["a", "b", "a", "b", "a", "b"], 2);
+    expect(words).toMatchObject([0, 1, 2, 3, 4]);
+  });
+  it("No repeated phrases", () => {
+    const words = findRepeatedPhrases(["red", "blue", "green", "yellow"], 2);
+    expect(words).toMatchObject([]);
+  });
+  it("Overlapping phrases", () => {
+    const words = findRepeatedPhrases(["a", "a", "a", "a"], 2);
+    expect(words).toMatchObject([0, 1, 2]);
+  });
+  it("Phrase length greater or equal to words length return []", () => {
+    const words = findRepeatedPhrases(["one", "two", "three"], 3);
+    expect(words).toMatchObject([]);
   });
 });
