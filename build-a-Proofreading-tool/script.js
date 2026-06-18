44bfe942cdc28fce94191e4f6e149357d95b7b73
sc-forlife <salemsc7@gmail.com>
2026-06-18 10:32:09 +0200

    working with repeatingWords function

--- build-a-Proofreading-tool/script.js
+++ build-a-Proofreading-tool/script.js
@@ -14,6 +14,32 @@ export function findPalindromeBreaks(wordsArr) {
   return indices;
 }
 
+function repeatingWords(words) {
+  const repeatingCheck = [];
+  const indices = [];
+  for (let i = 0; i < words.length - 1; i++) {
+    if (!repeatingCheck) {
+      repeatingCheck.push(words[i]);
+    } else {
+      if (repeatingCheck.includes(words[i])) {
+        indices.unshift(i);
+        indices.unshift(repeatingCheck.indexOf(words[i]));
+      } else {
+        repeatingCheck.push(words[i]);
+      }
+    }
+  }
+
+  return indices;
+}
+
 export function findRepeatedPhrases(words, phraseLength) {
-  if (phraseLength >= words.length) return [];
+  if (phraseLength >= words.length || phraseLength === 1) return [];
+  const arrPhrases = [];
+  for (let i = 0; i < words.length - 1; i++) {
+    arrPhrases.push(`${words[i]} ${words[i + 1]}`);
+  }
+  console.log(arrPhrases);
+  console.log(repeatingWords(arrPhrases).sort());
+  return repeatingWords(arrPhrases).sort();
 }
