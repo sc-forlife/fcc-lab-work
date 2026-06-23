322c5e01a7123dfc520459ed835e2d92e6a92d8d
sc-forlife <salemsc7@gmail.com>
2026-06-23 06:25:44 +0200

    Creating a function that returns grouped phraseLength words

--- build-a-Proofreading-tool/script.js
+++ build-a-Proofreading-tool/script.js
@@ -18,12 +18,16 @@ function repeatingWords(words) {
   const repeatingCheck = [];
   const indices = [];
   for (let i = 0; i < words.length - 1; i++) {
-    if (!repeatingCheck) {
+    console.log(i);
+
+    if (repeatingCheck.length === 0) {
       repeatingCheck.push(words[i]);
     } else {
       if (repeatingCheck.includes(words[i])) {
         indices.unshift(i);
+        console.log("inserting ", i);
         indices.unshift(repeatingCheck.indexOf(words[i]));
+        console.log("inserting first index ", repeatingCheck.indexOf(words[i]));
       } else {
         repeatingCheck.push(words[i]);
       }
@@ -34,7 +38,7 @@ function repeatingWords(words) {
 }
 
 export function findRepeatedPhrases(words, phraseLength) {
-  if (phraseLength >= words.length || phraseLength === 1) return [];
+  if (phraseLength >= words.length) return [];
   const arrPhrases = [];
   for (let i = 0; i < words.length - 1; i++) {
     arrPhrases.push(`${words[i]} ${words[i + 1]}`);
@@ -43,3 +47,22 @@ export function findRepeatedPhrases(words, phraseLength) {
   console.log(repeatingWords(arrPhrases).sort());
   return repeatingWords(arrPhrases).sort();
 }
+
+function groupedWords(words, length) {
+  const phraseLength = "";
+  const wordsArr = [];
+  for (let i = 0; i < words.length - 1; i++) {
+    wordsArr.push(words.slice(i, i + length).join(" "));
+  }
+
+  return wordsArr;
+}
+
+console.log(groupedWords(["the", "cat", "sat", "the", "cat"], 3));
+
+// const word = ["S", "a", "l", "e", "m"];
+
+// console.log(word.slice(0, 3).join(" "));
+// console.log(word.slice(2, 3).join(" "));
+// console.log(word.slice(2, 4).join(" "));
+// console.log(word.slice(3, 5).join(" "));
