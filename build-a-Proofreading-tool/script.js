2cb276edff6acedf9b5eae85ba1fa2fa3b647a1c
sc-forlife <salemsc7@gmail.com>
2026-06-23 10:52:04 +0200

    completed findRepeatedPhrases function

--- build-a-Proofreading-tool/script.js
+++ build-a-Proofreading-tool/script.js
@@ -14,41 +14,7 @@ export function findPalindromeBreaks(wordsArr) {
   return indices;
 }
 
-function repeatingWords(words) {
-  const repeatingCheck = [];
-  const indices = [];
-  for (let i = 0; i < words.length - 1; i++) {
-    console.log(i);
-
-    if (repeatingCheck.length === 0) {
-      repeatingCheck.push(words[i]);
-    } else {
-      if (repeatingCheck.includes(words[i])) {
-        indices.unshift(i);
-        console.log("inserting ", i);
-        indices.unshift(repeatingCheck.indexOf(words[i]));
-        console.log("inserting first index ", repeatingCheck.indexOf(words[i]));
-      } else {
-        repeatingCheck.push(words[i]);
-      }
-    }
-  }
-
-  return indices;
-}
-
-export function findRepeatedPhrases(words, phraseLength) {
-  if (phraseLength >= words.length) return [];
-  const arrPhrases = [];
-  for (let i = 0; i < words.length - 1; i++) {
-    arrPhrases.push(`${words[i]} ${words[i + 1]}`);
-  }
-  console.log(arrPhrases);
-  console.log(repeatingWords(arrPhrases).sort());
-  return repeatingWords(arrPhrases).sort();
-}
-
-function groupedWords(words, length) {
+function repeatingWords(words, length) {
   const phraseLength = "";
   const wordsArr = [];
   for (let i = 0; i < words.length - 1; i++) {
@@ -58,11 +24,25 @@ function groupedWords(words, length) {
   return wordsArr;
 }
 
-console.log(groupedWords(["the", "cat", "sat", "the", "cat"], 3));
+export function findRepeatedPhrases(phrasesArr, phraseLength) {
+  if (phraseLength >= phrasesArr.length) return [];
+  const words = repeatingWords(phrasesArr, phraseLength);
+  const repeatingIndex = [];
 
-// const word = ["S", "a", "l", "e", "m"];
-
-// console.log(word.slice(0, 3).join(" "));
-// console.log(word.slice(2, 3).join(" "));
-// console.log(word.slice(2, 4).join(" "));
-// console.log(word.slice(3, 5).join(" "));
+  for (let y = 0; y < words.length; y++) {
+    innerLoop: for (let i = 0; i < words.length; i++) {
+      if (y === i) {
+        continue;
+      } else {
+        if (words[y] === words[i]) {
+          repeatingIndex.push(i);
+        }
+      }
+    }
+  }
+  const sortedIndex = repeatingIndex.sort((a, b) => a - b);
+  const noRepeatIndex = sortedIndex.filter(
+    (num, index) => num !== repeatingIndex[index + 1],
+  );
+  return noRepeatIndex;
+}
