2a59e5a9706f49bccbfd90aa95b71d5382d211f9
sc-forlife <salemsc7@gmail.com>
2026-07-14 10:21:47 +0200

    created the page header function , and view category logic and functionality

--- build-a-bookmark-manager-app/script.js
+++ build-a-bookmark-manager-app/script.js
@@ -1,19 +1,21 @@
 const mainSection = document.getElementById("main-section");
 const formSection = document.getElementById("form-section");
+const bookmarkListSection = document.getElementById("bookmark-list-section");
 const addBookBtn = document.getElementById("add-bookmark-button");
 const closeFormBtn = document.getElementById("close-form-button");
 const categoryName = document.querySelector(".category-name");
 const categoryDropdown = document.getElementById("category-dropdown");
+const addBookmartsBtnForm = document.getElementById("add-bookmark-button-form");
+const viewCategoryBtn = document.getElementById("view-category-button");
+const nameInput = document.getElementById("name");
+const urlInput = document.getElementById("url");
+
+//bookmarks from LS
+const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
 
 //get the books from the local storage
 const getBookmarks = () => {
-  const books = JSON.parse(localStorage.getItem("bookmarks"));
-  //check if books exist
-  if (books) {
-    return books;
-  } else {
-    return [];
-  }
+  return bookmarks;
 };
 
 const displayOrCloseForm = () => {
@@ -21,15 +23,59 @@ const displayOrCloseForm = () => {
   formSection.classList.toggle("hidden");
 };
 
-addBookBtn.addEventListener("click", () => {
+const clearInputs = () => {
+  nameInput.value = "";
+  urlInput.value = "";
+};
+
+const displayOrHideCategory = () => {
+  mainSection.classList.toggle("hidden");
+  bookmarkListSection.classList.toggle("hidden");
+};
+
+const updatePageHeader = () => {
   const categorySelected =
     categoryDropdown.value.slice(0, 1).toUpperCase() +
     categoryDropdown.value.slice(1);
 
   categoryName.innerText = categorySelected;
+};
+
+// changes to form section
+addBookBtn.addEventListener("click", () => {
+  updatePageHeader();
   displayOrCloseForm();
 });
 
+//close form section
 closeFormBtn.addEventListener("click", () => {
   displayOrCloseForm();
+});
+
+addBookmartsBtnForm.addEventListener("click", () => {
+  //update the bookmarks in LS
+  //add array at the end
+
+  const bookmarkName = nameInput.value;
+  const category = categoryName.innerText;
+  const url = urlInput.value;
+
+  const objProperties = !bookmarkName || !category || !url;
+
+  if (objProperties) {
+    alert("Please provide all details");
+    return;
+  }
+  //add new obj in the localStorage
+  bookmarks.push({ name: bookmarkName, category: category, url: url });
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+  clearInputs();
+  displayOrCloseForm();
+});
+
+viewCategoryBtn.addEventListener("click", () => {
+  updatePageHeader();
+  //User stories
+  displayOrHideCategory();
 });
