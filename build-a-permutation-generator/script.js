56feb9a9579cc2c64bb081ad7481b33a454b16ae
sc-forlife <salemsc7@gmail.com>
2026-08-18 10:26:07 +0200

    Designing logic to display each permutation

--- build-a-permutation-generator/script.js
+++ build-a-permutation-generator/script.js
@@ -1,6 +1,25 @@
 const permuteString = (string, preValue = "", arr = []) => {
   if (string.length === 0) {
-    return arr.push(preValue);
+    console.log(preValue);
+    return;
   } else {
+    for (const char of string) {
+      newString = string.replace(char, "");
+      console.log(newString);
+      permuteString(newString, (preValue += char));
+
+      // console.log(preValue);
+    }
   }
 };
+
+console.log(permuteString("act"));
+
+//string
+
+//c + at
+//c + ta
+//a + ct
+//a + tc
+//t + ac
+//t + ca
